6f08e93d969272232d892eab738a56394f6d703f
Soeon Kim <developeon80@gmail.com>
2018-06-24 16:33:02 +0900

    @Aspect 애노테이션 기반으로 AOP 구현

---
+++ src/main/java/kr/hs/emirim/developeon/aopcal/main/MainXmlAspect.java
@@ -0,0 +1,21 @@
+package kr.hs.emirim.developeon.aopcal.main;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import kr.hs.emirim.developeon.aopcal.cal.Calculator;
+
+public class MainXmlAspect {
+
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:aopAspect.xml");
+		
+		Calculator impCal = ctx.getBean("impCal", Calculator.class);
+		long finveFact1 = impCal.factorial(5);
+		System.out.println("impcal.factorial(5) = " + finveFact1);
+		
+		Calculator recCal = ctx.getBean("recCal", Calculator.class);
+		long finveFact2 = recCal.factorial(5);
+		System.out.println("recCal.factorial(5) = " + finveFact2);
+	}
+
+}
